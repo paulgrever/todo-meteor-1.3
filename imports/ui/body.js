ca9123c1e98d978b0e484ea6188e9bba53b4dcbd
Paul Grever <paulgrever@gmail.com>
2016-07-07 15:14:33 -0600

    added reactive

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,5 @@
 import { Template } from 'meteor/templating';
-
+import { ReactiveDict} from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 
 import './body.html';
@@ -7,9 +7,23 @@ import './task.js'
 
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 
 Template.body.events({
   'submit .new-task'(event) {
@@ -28,5 +42,8 @@ Template.body.events({
 
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
